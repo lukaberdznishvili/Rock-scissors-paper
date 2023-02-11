bb987702c824b84ffa550c1cde72be19c405fb92
lukaberdznishvili <lukaberdznishvili24@gmail.com>
2023-02-11 20:59:33 +0300

    first commit

---
+++ rockScissorsPaper.js
@@ -0,0 +1,42 @@
+const playerText = document.querySelector("#playerText");
+const resultText = document.querySelector("#resultText");
+const computerText = document.querySelector("#computerText");
+const choiceBtns = document.querySelectorAll(".choicebtn");
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player : ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}))
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random()* 3) +1 ;
+    switch(randNum){
+        case 1:
+            computer = "Rock"
+            break;
+        case 2:
+            computer = "Paper"
+            break;
+        case 3:
+            computer = "Scissors"
+            break;
+    }
+}
+
+function checkWinner(){
+    if(player == computer){
+        return "Draw!"
+    }else if(computer == "Rock"){
+        return (player == "Paper") ? "You win!" : "You lose!"
+    }else if(computer == "Paper"){
+        return (player == "Scissors") ? "You win!" : "You lose!"
+    }else if(computer == "Scissors"){
+        return (player == "Rock") ? "You win!" : "You lose!"
+    }
+}
